05aa3749db33ca08a4a71896313bca7bfedfe37e
dannydsucab <166035474+dannydsucab@users.noreply.github.com>
2024-11-19 16:19:51 -0400

    Initial commit

---
+++ src/main/java/ve/edu/ucab/lab/Main.java
@@ -0,0 +1,19 @@
+package ve.edu.ucab.lab;
+
+public class Main {
+  protected Main() {
+  }
+
+  /**
+   * Método principal de la aplicación que se ejecuta al iniciar el programa.
+   * Es el punto de entrada de cualquier aplicación Java.
+   *
+   * @param args Argumentos de la línea de comandos pasados al programa.
+   *             En este caso, no se utilizan.
+   */
+  public static void main(final String[] args) {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    System.out.println("Hello UCAB!");
+  }
+}
